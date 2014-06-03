7b20600507f879e95fc83f43a77b8de19bfffdd2
phookajoe <smith_richard4@bah.com>
2014-06-03 08:12:54 -0600

    Community Install - Folder Affect

---
+++ components/com_community/templates/default/activities.system.topphotos.php
@@ -0,0 +1,34 @@
+<?php
+	$model		= CFactory::getModel( 'photos');
+	$photos		= $model->getPopularPhotos( 9 , 0 );
+
+	$tmpl   =	new CTemplate();
+?>
+<div class="cStream-Content">
+	<div class="cStream-Headline">
+		<b><?php echo JText::_('COM_COMMUNITY_ACTIVITIES_TOP_PHOTOS'); ?></b>
+	</div>
+	<div class="cStream-Attachment">
+		<div class="cStream-Photo top-photos row-fluid">
+		<?php
+		foreach( $photos as $photo )
+		{
+		?>
+			<b class="span3">
+				<a href="<?php echo $photo->getPhotoLink();?>" class="cSnip-Photo" title="<?php echo $this->escape($photo->caption);?>">
+					<?php 
+					$user = CFactory::getUser($photo->creator); 
+					?>
+					<img class="cPhoto-Thumb" alt="<?php echo $this->escape($photo->caption);?>" src="<?php echo $photo->getThumbURI();?>" />
+					<span>
+						<i><?php echo JText::sprintf('COM_COMMUNITY_PHOTOS_UPLOADED_BY' , $user->getDisplayName() );?></i>
+					</span>
+				</a>
+			</b>
+		<?php
+		}
+		?>
+		</div>
+	</div>
+	<?php $this->load('activities.actions'); ?>
+</div>
---
+++ logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-06-03 14:08:58 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-06-03T14:08:58+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2014-06-03T14:09:07+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
